0d141505a2bd0e81837a650300062dcda6c38ce3
naokiur <atomic.transfer.comp@gmail.com>
2017-09-29 15:55:13 +0900

    Try to add fail test for would liko confirm result of CircleCI when test is fail.

--- mysite/test_mysite.py
+++ mysite/test_mysite.py
@@ -1,8 +1,11 @@
+""" test """
 import unittest
 
 class TestMysite(unittest.TestCase):
+    """Test for my sitetest method"""
     def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        """ Test for my site. This will be error. """
+        self.assertEqual('foo'.upper(), 'foo')
 
 if __name__ == '__main__':
     unittest.main()
