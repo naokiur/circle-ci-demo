19342305c986e5e1cd09646d6acfeaeff461bc9c
naokiur <atomic.transfer.comp@gmail.com>
2017-09-29 17:11:07 +0900

    Fix pylint error.

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+""" module """
 import os
 import sys
 
@@ -12,6 +13,7 @@ if __name__ == "__main__":
         # exceptions on Python 2.
         try:
             import django
+            print(django)
         except ImportError:
             raise ImportError(
                 "Couldn't import Django. Are you sure it's installed and "
