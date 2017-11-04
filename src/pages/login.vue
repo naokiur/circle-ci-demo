72314b309f21423f75adfa8a5475af575efbfa42
naokiur <atomic.transfer.comp@gmail.com>
2017-11-04 18:28:57 +0900

    Change form name.

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -2,15 +2,15 @@
   <section>
     <h1>ログインページ</h1>
     <section class="contents">
-    <el-form ref="form" :model="form" label-width="6rem">
+    <el-form ref="form" :model="loginForm" label-width="6rem">
       <el-form-item label="ユーザ名">
-        <el-input v-model="form.name"></el-input>
+        <el-input v-model="loginForm.name"></el-input>
       </el-form-item>
       <el-form-item label="パスワード">
-        <el-input type="password" v-model="form.password"></el-input>
+        <el-input type="password" v-model="loginForm.password"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button @click="submitForm('form')">ログイン</el-button>
+        <el-button @click="submitForm('loginForm')">ログイン</el-button>
       </el-form-item>
     </el-form>
     </section>
@@ -23,7 +23,7 @@ export default {
   name: 'Login',
   data () {
     return {
-      form: {
+      loginForm: {
         name: '',
         password: ''
       }
