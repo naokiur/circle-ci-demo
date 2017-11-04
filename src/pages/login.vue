4696b2bf42f4025b228470a837789b64be1d4c2a
naokiur <atomic.transfer.comp@gmail.com>
2017-11-04 18:51:05 +0900

    Validation for login page.

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -2,11 +2,11 @@
   <section>
     <h1>ログインページ</h1>
     <section class="contents">
-    <el-form ref="form" :model="loginForm" label-width="6rem">
-      <el-form-item label="ユーザ名">
+    <el-form :model="loginForm" :rules="rules" ref="loginForm" label-width="6rem">
+      <el-form-item label="ユーザ名" prop="name">
         <el-input v-model="loginForm.name"></el-input>
       </el-form-item>
-      <el-form-item label="パスワード">
+      <el-form-item label="パスワード" prop="password">
         <el-input type="password" v-model="loginForm.password"></el-input>
       </el-form-item>
       <el-form-item>
@@ -26,13 +26,28 @@ export default {
       loginForm: {
         name: '',
         password: ''
+      },
+      rules: {
+        name: [
+          { required: true, message: 'Need to input username', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: 'Need to input password', trigger: 'blur' }
+        ]
       }
-
     }
   },
   methods: {
     submitForm (formName) {
-      console.dir(formName)
+      console.log(this.$refs[formName])
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!!')
+        } else {
+          alert('error')
+          return false
+        }
+      })
     }
   }
 }
