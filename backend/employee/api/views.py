20aba710db822ec334c605e1854699df6469c97e
naokiur <atomic.transfer.comp@gmail.com>
2017-12-26 08:21:30 +0900

    Try to add custom middleware for log.

--- backend/employee/api/views.py
+++ backend/employee/api/views.py
@@ -9,6 +9,16 @@ from employee.api.serializers import UserSerializer, GroupSerializer
 from employee.common.constants import LOGGER_NAME
 
 
+class Dog:
+	def __init__(self, name):
+		self.name = name
+
+
+class Cat:
+	def __init__(self, name):
+		self.name = name
+
+
 class UserViewSet(viewsets.ModelViewSet):
 	queryset = User.objects.all().order_by('-date_joined')
 	serializer_class = UserSerializer
@@ -17,10 +27,28 @@ class UserViewSet(viewsets.ModelViewSet):
 	def list(self, request):
 		self.log.info("ユーザ取得機能開始")
 
+		dog_list = [
+			Dog("A"),
+			Dog("B"),
+			Dog("C"),
+			Dog("D"),
+			Dog("E"),
+		]
+
+		cat_list = [
+			Cat("A"),
+			Cat("B"),
+			Cat("C"),
+			Cat("D"),
+			Cat("E"),
+		]
+
 		# 何かの処理
 		time.sleep(1)
 
 		print("users, execute something.")
+		print([x.name for x in dog_list])
+		print([x.name for x in cat_list])
 
 		# 何かの処理
 		time.sleep(1)
---
+++ backend/employee/common/interceptor/log_middleware.py
@@ -0,0 +1,13 @@
+
+class LogMiddleware:
+	def __init__(self, get_response):
+		self.get_response = get_response
+
+	def __call__(self, request):
+		print("Before middleware")
+
+		response = self.get_response(request)
+
+		print("After middleware")
+
+		return response
