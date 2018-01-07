04651a6c812f2a307844d085715f00349750a17e
naokiur <atomic.transfer.comp@gmail.com>
2018-01-07 14:13:38 +0900

    End training.

--- front/employee/src/js/training/main.js
+++ front/employee/src/js/training/main.js
@@ -1,13 +1,13 @@
-import {
-  default as firstFunction,
-  secondFunction
-  // otherFunc
-} from './ReIntroduction'
+// import {
+//   default as firstFunction,
+//   secondFunction
+//   // otherFunc
+// } from './ReIntroduction'
 
 export default function () {
-  console.log('----- begin training -----')
-  firstFunction()
-  secondFunction()
+  // console.log('----- begin training -----')
+  // firstFunction()
+  // secondFunction()
   // otherFunc() cannot call because this function is not exported.
-  console.log('----- end training -----')
+  // console.log('----- end training -----')
 }
