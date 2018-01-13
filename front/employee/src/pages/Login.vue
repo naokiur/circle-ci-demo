7f730dc0b8eb6607a5fceb62062299f481ce13e7
naokiur <atomic.transfer.comp@gmail.com>
2018-01-13 22:06:02 +0900

    Suitable code line.

--- front/employee/src/pages/Login.vue
+++ front/employee/src/pages/Login.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
-    <el-input type="text" v-model="userName" name="userName" placeholder="ユーザ名"></el-input>
-    <el-input type="password" v-model="password" name="password" placeholder="パスワード"></el-input>
+    <el-input
+      type="text"
+      v-model="userName"
+      name="userName"
+      placeholder="ユーザ名">
+    </el-input>
+    <el-input
+      type="password"
+      v-model="password"
+      name="password"
+      placeholder="パスワード">
+    </el-input>
     <el-button @click="login">ログイン</el-button>
   </div>
 </template>
