c90466fe7ca66798676474b5fe65d2e80b965cac
naokiur <atomic.transfer.comp@gmail.com>
2018-02-02 17:54:03 +0900

    Add module and models.

--- backend/employee/api/models.py
+++ backend/employee/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    user_id = models.CharField(max_length=12)
+    password = models.TextField()
+    first_name = models.TextField()
+    last_name = models.TextField()
+    post_code = models.CharField(max_length=10)
+    age = models.IntegerField()
+    enter_date = models.DateField()
+
+    class Meta:
+        managed = True
+        db_table = 'employee'
--- backend/employee/settings.py
+++ backend/employee/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'employee',
+    'employee.api',
 ]
 
 REST_FRAMEWORK = {
