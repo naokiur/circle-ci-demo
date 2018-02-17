9770ee1d27ec9bf04cdaaa68427c6b6b70c32239
naokiur <atomic.transfer.comp@gmail.com>
2018-02-17 09:05:50 +0900

    [refactor]Remove redundant source code.

--- backend/employee/api/views.py
+++ backend/employee/api/views.py
@@ -8,51 +8,21 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 
 
-class Dog:
-    def __init__(self, name):
-        self.name = name
-
-
-class Cat:
-    def __init__(self, name):
-        self.name = name
-
-
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     log = getLogger(LOGGER_NAME)
 
     def list(self, request):
-        self.log.info("ユーザ取得機能開始")
-
-        dog_list = [
-            Dog("A"),
-            Dog("B"),
-            Dog("C"),
-            Dog("D"),
-            Dog("E"),
-        ]
-
-        cat_list = [
-            Cat("A"),
-            Cat("B"),
-            Cat("C"),
-            Cat("D"),
-            Cat("E"),
-        ]
 
         # 何かの処理
         time.sleep(1)
 
         print("users, execute something.")
-        print([x.name for x in dog_list])
-        print([x.name for x in cat_list])
 
         # 何かの処理
         time.sleep(1)
 
-        self.log.info("ユーザ取得機能終了")
         return Response()
 
 
