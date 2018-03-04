c41bc84a23f815d508bfeee1333cabf73472908f
naokiur <atomic.transfer.comp@gmail.com>
2018-03-04 23:50:12 +0900

    [feat]Try to add constants file, but it is not correct.

---
+++ frontend/employee/src/js/constants.js
@@ -0,0 +1,13 @@
+// export const MenuIndex = {
+//   Register: Symbol(1),
+//   Search: Symbol(2)
+// }
+
+export const MenuIndex = {
+  Register: Symbol(1),
+  Search: Symbol(2)
+}
+
+export const func = function () {
+  return 'aaa'
+}
