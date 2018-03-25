02e0aa6611f0669d42bf93219e7b496bbeb7f178
naokiur <atomic.transfer.comp@gmail.com>
2018-03-25 16:17:25 +0900

    [feat]Remove redundant source code.

--- frontend/employee/src/router.js
+++ frontend/employee/src/router.js
@@ -8,12 +8,6 @@ export default {
       component: () => import(/* webpackChunkName: "login" */'./pages/Login.vue')
     },
     {
-      path: '/search',
-      name: 'search',
-      meta: {title: '検索画面'},
-      component: () => import(/* webpackChunkName: "menu" */'./pages/Search.vue')
-    },
-    {
       path: '/index',
       name: 'index',
       meta: {title: 'NEMS'},
