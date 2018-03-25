a08e0fe8286d7ccc4b18d10a8fe9af4c1393a927
naokiur <atomic.transfer.comp@gmail.com>
2018-03-25 16:27:12 +0900

    [fix]Change Login screen design, it is not suitable before.

--- frontend/employee/src/pages/Login.vue
+++ frontend/employee/src/pages/Login.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <section class="TopTitle">
+      <h2>社員管理システム</h2>
+      <h3> - <span class="abbr">E</span>mployee <span class="abbr">M</span>anagement <span class="abbr">S</span>ystem - </h3>
+    </section>
     <section class="loginArea">
       <el-input
         type="text"
@@ -41,9 +45,19 @@
 </script>
 
 <style scoped>
+  .TopTitle {
+    background-color: #5794BC;
+    color: #FFFFFF;
+    margin: 1rem 10rem;
+  }
+
   .loginArea {
     width: 25rem;
     margin: auto;
+  }
+
+  .abbr {
+    color: #FFD04B;
   }
 
 
