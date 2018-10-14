4b0d22f7f7a3e73318e367fbaea367c5fff55cbf
naokiur <atomic.transfer.comp@gmail.com>
2018-10-14 18:33:00 +0900

    [feat]Adjust layout.

--- frontend/employee/src/pages/Register.vue
+++ frontend/employee/src/pages/Register.vue
@@ -2,47 +2,45 @@
   <div>
     <section>
       <el-row justify="center" type="flex">
-        <el-col :span="12">
-          <el-row class="employeeInformarion" justify="center" type="flex">
-            <el-col>
-              <h2>新規登録</h2>
-              <el-form label-width="8rem">
-                <el-form-item label="名前">
-                  <el-row type="flex" :gutter="20">
-                    <el-col :span="12">
-                      <el-input v-model="firstName"></el-input>
-                    </el-col>
-                    <el-col :span="12">
-                      <el-input v-model="lastName"></el-input>
-                    </el-col>
-                  </el-row>
-                </el-form-item>
-                <el-form-item label="部署">
-                  <el-select v-model="selectedPost">
-                    <el-option
-                      v-for="post in postList"
-                      :value="post.value"
-                      :key="post.key"
-                      :label="post.value">
-                    </el-option>
-                  </el-select>
-                </el-form-item>
-                <el-form-item label="年齢">
-                  <el-input-number size="mini" :controls="false" v-model="age" :min="0"></el-input-number>
-                </el-form-item>
-                <el-form-item label="入社年月日">
-                  <el-row type="flex" :gutter="20">
-                    <el-col :span="10">
-                      <el-date-picker type="date" v-model="enterDate"/>
-                    </el-col>
-                  </el-row>
-                </el-form-item>
-                <el-button @click="register" round>
-                  登録
-                </el-button>
-              </el-form>
-            </el-col>
-          </el-row>
+        <el-col :span="12" class="employeeInformarion">
+          <h2>新規</h2>
+          <el-form label-width="8rem">
+            <el-form-item label="名前">
+              <el-row type="flex" :gutter="20">
+                <el-col :span="10">
+                  <el-input v-model="firstName"></el-input>
+                </el-col>
+                <el-col :span="10">
+                  <el-input v-model="lastName"></el-input>
+                </el-col>
+              </el-row>
+            </el-form-item>
+            <el-form-item label="部署">
+              <el-col :span="9">
+                <el-select v-model="selectedPost">
+                  <el-option
+                    v-for="post in postList"
+                    :value="post.value"
+                    :key="post.key"
+                    :label="post.value">
+                  </el-option>
+                </el-select>
+              </el-col>
+            </el-form-item>
+            <el-form-item label="年齢">
+              <el-col :span="3">
+                <el-input-number size="mini" :controls="false" v-model="age" :min="0"></el-input-number>
+              </el-col>
+            </el-form-item>
+            <el-form-item label="入社年月日">
+              <el-col :span="3">
+                <el-date-picker type="date" v-model="enterDate"/>
+              </el-col>
+            </el-form-item>
+            <el-button @click="register" round >
+              登録
+            </el-button>
+          </el-form>
         </el-col>
       </el-row>
     </section>
@@ -91,9 +89,8 @@
 
 <style scoped>
   .employeeInformarion {
-    width: 50%;
-    padding: 0.5rem 1rem;
-    /* margin: auto; */
+    padding: 1rem;
+    margin: 1rem;
     border-bottom: 0.1rem #e6e6e6 solid;
     background-color: #FFFFFF;
   }
@@ -102,7 +99,10 @@
     width: 3rem;
   }
 
-  .el-col {
-    padding: 0;
+  .employeeInformarion >>> .el-button {
+    background-color: #5794BC;
+    color: #FFFFFF;
+    height: 100%;
+    width: 80%;
   }
 </style>
